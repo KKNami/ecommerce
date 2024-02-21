a229da4cfa06e3b77e3a5342b4abee0272479aba
Ahmet Kerem Kantarcı <kerem_kntrc26@hotmail.com>
2024-02-21 22:59:36 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home-Files/Home";
+import { Login } from "./pages/Login-Files/Login";
+import { Routes, Route } from "react-router-dom";
+import { UserInfo } from "./pages/User.Info-Files/User.Info";
+import { Cart } from "./pages/Cart-Files/Cart";
+import { Shop } from "./pages/Shop-Files/Shop";
+import { ShopContextProvider } from "./context/Shop-Context";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ShopContextProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/userinfo" element={<UserInfo />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/Shop" element={<Shop />} />
+        </Routes>
+      </ShopContextProvider>
     </div>
   );
 }
